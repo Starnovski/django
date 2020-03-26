2e56215c01498c242e15d64cb6ef6009289acbc2
Starnovski <pawelsz1818@wp.pl>
2020-03-26 21:54:18 +0100

    First model and view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+
+def post_index(request):
+    posts = Post.objects.all()
+    return render(request, '../templates/post_index.html', {'posts':posts})

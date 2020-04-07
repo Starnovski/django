788ef04f738efd9c3841a97b08727f690e05ac51
Starnovski <pawelsz1818@wp.pl>
2020-04-07 14:04:20 +0200

    Added simple authentication, using Django buit-in views

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 # Create your views here.
 urlpatterns = [
     path('', views.post_index, name = 'post_index'),
-    path('post/<int:pk>', views.post_show, name = 'post_show')
+    path('post/<int:pk>', views.post_show, name = 'post_show'),
 ]

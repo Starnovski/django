2aaea991d918e47d9c43ae0c67497b4ad5bdb999
Starnovski <pawelsz1818@wp.pl>
2020-04-10 17:36:02 +0200

    Added comment form

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.post_index, name = 'post_index'),
     path('post/<int:pk>', views.post_show, name = 'post_show'),
+    path('post/<int:pk>/new_comment', views.comment_new, name = 'comment_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -23,3 +24,14 @@ def post_show(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.all()
     return render(request, '../templates/post_show.html', {'post':post, 'comments':comments})
+
+
+def comment_new(request, pk):
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.save()
+    else:
+        form = CommentForm()
+    return render(request, '../templates/comment_new.html',{'form':form})
